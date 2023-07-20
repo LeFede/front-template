ea414c3bf994ca6aa44925c19976aae37cbcd797
LeFede <lefedeok@gmail.com>
2023-07-20 17:40:37 -0300

    asd

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+        <a href="https://vitejs.dev" target="_blank" rel="">
           <img src={react} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank" rel="noreferrer">
