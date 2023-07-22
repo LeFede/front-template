4276697bde122400c35df91bd24de1660e5a6be1
LeFede <lefedeok@gmail.com>
2023-07-22 10:32:43 -0300

    hello

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -22,10 +22,22 @@ module.exports = {
   },
   plugins: ['react'],
   rules: {
-    'no-console': ['error'],
     'react/react-in-jsx-scope': 'off',
     '@typescript-eslint/explicit-function-return-type': ['off'],
     'no-multiple-empty-lines': ['error', { max: 1, maxEOF: 0 }],
+    'no-console': ['error'],
+    'react/jsx-tag-spacing': ['error'],
+    'max-len': [
+      'error',
+      {
+        code: 80,
+        // tabWidth: 2,
+        // "ignoreComments": true, //"comments": 80
+        // "ignoreUrls": true,
+        // "ignoreStrings": true,
+        ignoreTemplateLiterals: true,
+      },
+    ],
   },
   ignorePatterns: ['.eslintrc.cjs', 'vite.config.ts'],
   settings: {
--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,7 @@ function App() {
           <img src={vite} className="logo react" alt="React logo" />
         </a>
       </div>
+
       <h1>Vite + React</h1>
       <div className="card">
         <button
@@ -35,5 +36,4 @@ function App() {
     </>
   )
 }
-
 export default App
